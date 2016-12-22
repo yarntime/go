1b78406c47f4eb6d8a48943fa6e05140a0bdfeeb
yarntime <yarntime@163.com>
2016-12-22 10:41:27 +0800

    bug fix

--- go/async.go
+++ go/async.go
@@ -2,20 +2,29 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
 var ch = make(chan int)
+var finishCount int
+var l sync.Mutex
 
 func main() {
+
+	finishCount = 0
 
 	for i := 0; i < 4; i++ {
 		go worker(i)
 	}
 
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 100000; i++ {
 		ch <- i
 	}
+
+	time.Sleep(time.Second)
+	fmt.Printf("finish count is %d\n", finishCount)
+	close(ch)
 }
 
 func worker(i int) {
@@ -23,7 +32,10 @@ func worker(i int) {
 		select {
 		case num := <-ch:
 			fmt.Printf("worker %d print %d\n", i, num)
-			time.Sleep(time.Second)
+			time.Sleep(time.Nanosecond * 1000)
+			l.Lock()
+			finishCount++
+			l.Unlock()
 		}
 	}
 }
