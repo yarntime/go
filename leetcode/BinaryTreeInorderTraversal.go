538011bafc3b9c35b4484e5d4fdc7017e183f233
yarntime@163.com <yarntime@163.com>
2018-03-10 10:26:05 +0800

    add inorder traversal

---
+++ leetcode/BinaryTreeInorderTraversal.go
@@ -0,0 +1,10 @@
+func inorderTraversal(root *TreeNode) []int {
+    result := make([]int, 0)
+    if root == nil {
+        return result
+    }
+    result = append(result, inorderTraversal(root.Left)...)
+    result = append(result, root.Val)
+    result = append(result, inorderTraversal(root.Right)...)
+    return result
+}
