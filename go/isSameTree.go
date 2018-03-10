70335b4c2a5753d9107606418942e6a633d0c84f
yarntime@163.com <yarntime@163.com>
2018-03-10 16:56:43 +0800

    add is same tree

---
+++ go/isSameTree.go
@@ -0,0 +1,12 @@
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+    if p == nil && q == nil {
+        return true
+    }
+    if (p == nil && q != nil) || (p != nil && q == nil) {
+        return false
+    }
+    if p.Val == q.Val && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right) {
+        return true
+    }
+    return false
+}
