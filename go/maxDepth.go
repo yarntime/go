4b28dfa0c3527960a45343d86a887b100cd336e5
yarntime <yarntime@163.com>
2018-03-12 18:40:58 +0800

    add max depth

---
+++ go/maxDepth.go
@@ -0,0 +1,13 @@
+func maxDepth(root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+    return max(maxDepth(root.Left), maxDepth(root.Right)) + 1
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
