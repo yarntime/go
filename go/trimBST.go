41c0b4994266ee6d84554a75c0d8bf4434a9135d
yarntime <yarntime@163.com>
2018-03-14 11:55:23 +0800

    add trim bst

---
+++ go/trimBST.go
@@ -0,0 +1,14 @@
+func trimBST(root *TreeNode, L int, R int) *TreeNode {
+    if root == nil {
+        return nil
+    }
+    if root.Val < L {
+        return trimBST(root.Right, L, R)
+    }
+    if root.Val > R {
+        return trimBST(root.Left, L, R)
+    }
+    root.Left = trimBST(root.Left, L, R)
+    root.Right = trimBST(root.Right, L, R)
+    return root
+}
