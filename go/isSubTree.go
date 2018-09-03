d67b2cc1d21df8153d5a8ac9c5b030f852c9faa8
yarntime <yarntime@163.com>
2018-09-03 09:49:50 +0800

    Add isSubTree

---
+++ go/isSubTree.go
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func isSubtree(s *TreeNode, t *TreeNode) bool {
+    if t == nil {
+        return true
+    }
+    return s != nil && (isSameTree(s, t) || isSubtree(s.Left, t) || isSubtree(s.Right, t))
+}
+
+func isSameTree(s *TreeNode, t *TreeNode) bool {
+    if s == nil && t == nil {
+        return true
+    }
+    if s == nil || t == nil {
+        return false
+    }
+    if s.Val != t.Val {
+        return false
+    }
+    return isSameTree(s.Left, t.Left) && isSameTree(s.Right, t.Right)
+}
