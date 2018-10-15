83034fa84432f98e3bba45e231679ba7a7828e1a
yarntime <yarntime@163.com>
2018-10-15 15:49:21 +0800

    add middleOfNode

---
+++ go/middleOfNode.go
@@ -0,0 +1,8 @@
+func middleNode(head *ListNode) *ListNode {
+    slow, fast := head, head
+    for fast != nil && fast.Next != nil {
+        slow = slow.Next
+        fast = fast.Next.Next
+    }
+    return slow
+}
