2940b337c1a306b75658348b277c7979e8d29675
yarntime <yarntime@163.com>
2018-10-29 10:39:35 +0800

    update sortColors

--- go/sortColors.go
+++ go/sortColors.go
@@ -1,28 +1,15 @@
 func sortColors(nums []int)  {
-    red, white, blue := 0, 0, 0
+    colors := [3]int{}
     for i := 0; i < len(nums); i++ {
-        if nums[i] == 0 {
-            red++
-        } else if nums[i] == 1 {
-            white++
-        } else {
-            blue++
-        }
+        colors[nums[i]]++
     }
+    
     start := 0
-    for red > 0 {
-        nums[start] = 0
-        start++
-        red--
-    }
-    for white > 0 {
-        nums[start] = 1
-        start++
-        white--
-    }
-    for blue > 0 {
-        nums[start] = 2
-        start++
-        blue--
+    for i := 0; i < 3; i++ {
+        for colors[i] > 0 {
+            nums[start] = i
+            colors[i]--
+            start++
+        }
     }
 }
