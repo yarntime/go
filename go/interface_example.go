e5c8247deb23b6f7ac07fc5a4f643e853c51fa35
yarntime <yarntime@163.com>
2018-11-25 21:44:54 +0800

    add interface example

---
+++ go/interface_example.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Role interface {
+	Do()
+}
+
+type Teacher struct {
+}
+
+func (t *Teacher) Do() {
+	fmt.Printf("I'm a teacher\n")
+}
+
+type Student struct {
+}
+
+func (s *Student) Do() {
+	fmt.Printf("I'm a student\n")
+}
+
+var _ Role = new(Teacher)
+
+var _ Role = new(Student)
+
+func Do(v interface{}) {
+	t, ok := v.(Role)
+	if !ok {
+		fmt.Printf("type mismatched.\n")
+	}
+	t.Do()
+}
+
+func main() {
+	t := &Teacher{}
+	Do(t)
+
+	s := &Student{}
+	Do(s)
+}
