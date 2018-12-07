944d7ed5cf9a223b6284977d9ffd468af4e50f42
yarntime <yarntime@163.com>
2018-12-07 10:04:45 +0800

    add cpp lowestCommonAncestor

--- go/lowestCommonAncestor.go
+++ go/lowestCommonAncestor.go
@@ -1,3 +1,29 @@
+/*
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if (root == NULL) {
+            return NULL;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
+        TreeNode *l = lowestCommonAncestor(root->left, p, q);
+        TreeNode *r = lowestCommonAncestor(root->right, p, q);
+        if (l != NULL && r != NULL && l != r) {
+            return root;
+        } else if (l != NULL) {
+            return lowestCommonAncestor(root->left, p, q);
+        } else if (r != NULL) {
+            return lowestCommonAncestor(root->right, p, q);
+        } else {
+            return NULL;
+        }
+    }
+    
+};
+*/
+
 type TreeNode struct {
 	Val int
 	Left  *TreeNode
